e06c566998c48bcc85735bd988387eb3414af862
naggyman <morgan@french.net.nz>
2016-09-27 17:23:47 +1300

    update 27/09

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
  
-
 /* Code for Assignment ?? 
  * Name:
  * Usercode:
  * ID:
  */
 
-
 import ecs100.*;
 import java.util.*;
 import java.io.*;
 import java.awt.*;
-
 
 /** <description of class Main>
  */
@@ -22,10 +19,10 @@ public class Main{
     private ToolPath tool_path;
     // state of the GUI
     private int state; // 0 - nothing
-                       // 1 - inverse point kinematics - point
-                       // 2 - enter path. Each click adds point  
-                       // 3 - enter path pause. Click does not add the point to the path
-    
+    // 1 - inverse point kinematics - point
+    // 2 - enter path. Each click adds point  
+    // 3 - enter path pause. Click does not add the point to the path
+
     /**      */
     public Main(){
         UI.initialise();
@@ -35,126 +32,133 @@ public class Main{
         UI.addButton("Load path XY", this::load_xy);
         UI.addButton("Save path Ang", this::save_ang);
         UI.addButton("Load path Ang:Play", this::load_ang);
-                
-       // UI.addButton("Quit", UI::quit);
+        UI.addButton("Save PWM", this::save_pwm);
+        UI.addButton("Print PWM", this::print_pwm);
+
+        // UI.addButton("Quit", UI::quit);
         UI.setMouseMotionListener(this::doMouse);
         UI.setKeyListener(this::doKeys);
 
-
         //ServerSocket serverSocket = new ServerSocket(22); 
-        
         this.arm = new Arm();
         this.drawing = new Drawing();
+        this.tool_path = new ToolPath();
         this.run();
         arm.draw();
     }
-    
+
     public void doKeys(String action){
         UI.printf("Key :%s \n", action);
         if (action.equals("b")) {
             // break - stop entering the lines
             state = 3;
             //
-          
+
         }
-               
+
     }
-    
-    
+
     public void doMouse(String action, double x, double y) {
-         //UI.printf("Mouse Click:%s, state:%d  x:%3.1f  y:%3.1f\n",
-         //   action,state,x,y);
+        //UI.printf("Mouse Click:%s, state:%d  x:%3.1f  y:%3.1f\n",
+        //   action,state,x,y);
         UI.clearGraphics();
         String out_str=String.format("%3.1f %3.1f",x,y);
         UI.drawString(out_str, x+10,y+10);
-         // 
-         if ((state == 1)&&(action.equals("clicked"))){
-          // draw as 
-          
-          arm.inverseKinematic(x,y);
-          arm.draw();
-          return;
+        // 
+        if ((state == 1)&&(action.equals("clicked"))){
+            // draw as 
+
+            arm.inverseKinematic(x,y);
+            arm.draw();
+            return;
         }
-        
-         if ( ((state == 2)||(state == 3))&&action.equals("moved") ){
-          // draw arm and path
-          arm.inverseKinematic(x,y);
-          arm.draw();
-         
-          // draw segment from last entered point to current mouse position
-          if ((state == 2)&&(drawing.get_path_size()>0)){
-            PointXY lp = new PointXY();
-            lp = drawing.get_path_last_point();
-            //if (lp.get_pen()){
-               UI.setColor(Color.GRAY);
-               UI.drawLine(lp.get_x(),lp.get_y(),x,y);
-           // }
-          }
-           drawing.draw();
+
+        if ( ((state == 2)||(state == 3))&&action.equals("moved") ){
+            // draw arm and path
+            arm.inverseKinematic(x,y);
+            arm.draw();
+
+            // draw segment from last entered point to current mouse position
+            if ((state == 2)&&(drawing.get_path_size()>0)){
+                PointXY lp = new PointXY();
+                lp = drawing.get_path_last_point();
+                //if (lp.get_pen()){
+                UI.setColor(Color.GRAY);
+                UI.drawLine(lp.get_x(),lp.get_y(),x,y);
+                // }
+            }
+            drawing.draw();
         }
-        
+
         // add point
         if (   (state == 2) &&(action.equals("clicked"))){
             // add point(pen down) and draw
             UI.printf("Adding point x=%f y=%f\n",x,y);
             drawing.add_point_to_path(x,y,true); // add point with pen down
-            
+
             arm.inverseKinematic(x,y);
             arm.draw();
             drawing.draw();
             drawing.print_path();
         }
-        
-        
+
         if (   (state == 3) &&(action.equals("clicked"))){
             // add point and draw
             //UI.printf("Adding point x=%f y=%f\n",x,y);
             drawing.add_point_to_path(x,y,false); // add point wit pen up
-            
+
             arm.inverseKinematic(x,y);
             arm.draw();
             drawing.draw();
             drawing.print_path();
             state = 2;
         }
-        
-        
+
     }
-   
     
     public void save_xy(){
         state = 0;
         String fname = UIFileChooser.save();
         drawing.save_path(fname);
     }
-    
+
     public void enter_path_xy(){
-         state = 2;
+        state = 2;
     }
-    
+
     public void inverse(){
-         state = 1;
-         arm.draw();
+        state = 1;
+        arm.draw();
     }
-    
+
     public void load_xy(){
         state = 0;
         String fname = UIFileChooser.open();
         drawing.load_path(fname);
         drawing.draw();
-        
+
         arm.draw();
     }
-    
+
     // save angles into the file
     public void save_ang(){
-        String fname = UIFileChooser.open();
+        String fname = UIFileChooser.save();
         tool_path.convert_drawing_to_angles(drawing,arm,fname);
+        tool_path.save_angles(fname);
+    }
+
+    public void load_ang(){
+    
     }
     
+    public void save_pwm(){
+        String fname = UIFileChooser.save();
+        tool_path.convert_angles_to_pwm(arm);
+        tool_path.save_angles(fname);
+    }
     
-    public void load_ang(){
-        
+    public void print_pwm(){
+        UI.println( arm.get_pwm1() + " " + arm.get_pwm2());
     }
     
     public void run() {
@@ -169,3 +173,4 @@ public class Main{
     }    
 
 }
+
